7a3fba344563cde26dbd8a3eb665651dc5203eaf
Freeman136 <a13661777@gmail.com>
2023-10-23 22:46:16 +0300

    create project

---
+++ Crush3/Crush3/Crush3App.swift
@@ -0,0 +1,17 @@
+//
+//  Crush3App.swift
+//  Crush3
+//
+//  Created by Andrew on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Crush3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
